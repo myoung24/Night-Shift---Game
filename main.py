d6efbce2deb7b4991588b7b2535ea82e08513f64
myoung24 <127060152+myoung24@users.noreply.github.com>
2023-03-16 13:06:57 -0230

    Add files via upload

--- main.py
+++ main.py
@@ -5,11 +5,22 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((800, 540))
+
+w, h = 950, 650
+screen = pygame.display.set_mode((w, h))
 pygame.display.set_caption('Night Shift')  # Title of the window
 clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 140)
 
-test_surface = pygame.image.load('clouds.webp')
+bg_black = pygame.Surface((w, h))
+bg_black.fill('black')
+
+bg_clouds = pygame.image.load('clouds.webp')
+bg_clouds.set_alpha(150)
+
+fg_test = pygame.image.load('template.jpeg')
+
+text_surface = test_font.render("NIGHT SHIFT", True, "white")
 
 
 while True:
@@ -18,7 +29,12 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(test_surface, (0, 0))
+    screen.blit(bg_black, (0, 0))
+    screen.blit(bg_clouds, (0, 0))
+    screen.blit(fg_test, (100, 300))
+    screen.blit(text_surface, (180, 110))
+
+
 
 
     pygame.display.update()
